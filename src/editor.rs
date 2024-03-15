2b2b4a606a68f194b54961eb8ee821bfb5f33e03
Jacob Sun <jacobsun000@gmail.com>
2024-03-15 04:57:43 -0500

    feat: Prevent cursor to move beyond screen boundary

--- src/editor.rs
+++ src/editor.rs
@@ -89,11 +89,14 @@ impl Editor {
 
     fn move_cursor(&mut self, key: Key) {
         let Pos { mut x, mut y } = self.cursor_pos;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
         match key {
-            Key::Up => y = y.saturating_sub(1),
-            Key::Down => y = y.saturating_add(1),
-            Key::Left => x = x.saturating_sub(1),
-            Key::Right => x = x.saturating_add(1),
+            Key::Up => y = y.saturating_sub(1).max(0),
+            Key::Down => y = y.saturating_add(1).min(height),
+            Key::Left => x = x.saturating_sub(1).max(0),
+            Key::Right => x = x.saturating_add(1).min(width),
             _ => (),
         }
         self.cursor_pos = Pos { x, y };
