faf22488a232590614e375fef8f67307299185fe
Jacob Sun <jacobsun000@gmail.com>
2024-03-15 04:36:46 -0500

    feat: Welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -2,6 +2,8 @@ use crate::Terminal;
 use std::io;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     terminal: Terminal,
     should_quit: bool,
@@ -55,10 +57,25 @@ impl Editor {
     }
 
     fn draw_rows(&self) {
-        for i in 0..self.terminal.size().height - 1 {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            println!("{i}\r");
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
+    }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("RVim editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
     }
 }
 
